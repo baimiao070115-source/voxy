a49df5bd80a3f9160d4c605a834d3dcad3681b71
kkHubq4xvMC6Tjhkg0jtTSBbeZ6REYL7SFvB <mail@felix-kuhlmann.de>
2026-01-14 21:43:08 +0100

    Port Locale Fixes (45ff6c44), Rename (e86beefb) and Client Storage Optimization (3cc5afc1)

--- src/main/java/me/cortex/voxy/client/core/gl/Capabilities.java
+++ src/main/java/me/cortex/voxy/client/core/gl/Capabilities.java
@@ -7,6 +7,8 @@ import org.lwjgl.opengl.GL20C;
 import org.lwjgl.opengl.GL30;
 import org.lwjgl.system.MemoryUtil;
 
+import java.util.Locale;
+
 import static org.lwjgl.opengl.GL11.GL_NEAREST;
 import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
 import static org.lwjgl.opengl.GL11.GL_TEXTURE_MAG_FILTER;
@@ -86,8 +88,8 @@ public class Capabilities {
 
         this.ssboMaxSize = glGetInteger64(GL_MAX_SHADER_STORAGE_BLOCK_SIZE);
 
-        this.isMesa = glGetString(GL_VERSION).toLowerCase().contains("mesa");
-        var vendor = glGetString(GL_VENDOR).toLowerCase();
+        this.isMesa = glGetString(GL_VERSION).toLowerCase(Locale.ROOT).contains("mesa");
+        var vendor = glGetString(GL_VENDOR).toLowerCase(Locale.ROOT);
         this.isIntel = vendor.contains("intel");
         this.isNvidia = vendor.contains("nvidia");
         this.isAmd = vendor.contains("amd")||vendor.contains("radeon");
