64f60c0ee91b8b68aaea5f3a30a421a00dc1c727
kkHubq4xvMC6Tjhkg0jtTSBbeZ6REYL7SFvB <mail@felix-kuhlmann.de>
2026-01-14 23:24:08 +0100

    Port Replacements API (53f85777) and Timing fixes (f887b1ac)

--- src/main/java/me/cortex/voxy/client/core/gl/shader/Shader.java
+++ src/main/java/me/cortex/voxy/client/core/gl/shader/Shader.java
@@ -69,6 +69,7 @@ public class Shader extends TrackedObject {
             J make(Builder<J> builder, int program);
         }
         final Map<String, String> defines = new HashMap<>();
+        final Map<String, String> replacements = new LinkedHashMap<>();
         private final Map<ShaderType, String> sources = new HashMap<>();
         private final IShaderProcessor processor;
         private final IShaderObjectConstructor<T> constructor;
@@ -119,6 +120,11 @@ public class Shader extends TrackedObject {
             return this;
         }
 
+        public Builder<T> replace(String value, String replacement) {
+            this.replacements.put(value, replacement);
+            return this;
+        }
+
         public Builder<T> add(ShaderType type, String id) {
             this.addSource(type, ShaderLoader.parse(id));
             return this;
@@ -143,6 +149,10 @@ public class Shader extends TrackedObject {
                     src = src.substring(0, src.indexOf('\n')+1) +
                             defs
                             + src.substring(src.indexOf('\n')+1);
+
+                    for (var replacement : this.replacements.entrySet()) {
+                        src = src.replaceAll(replacement.getKey(), replacement.getValue());
+                    }
 
                     shaders[i++] = createShader(entry.getKey(), src);
                 }
